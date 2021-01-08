aa200c09eb27ed4591c5381a2f5b6ec8c016072d
gangslee <grandnet1225@gmail.com>
2021-01-08 15:07:54 +0900

    UIHostingViewController Tutorial

---
+++ HostingControllerDemo/SwiftUIView.swift
@@ -0,0 +1,29 @@
+//
+//  SwiftUIView.swift
+//  HostingControllerDemo
+//
+//  Created by 이경수 on 2021/01/08.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var text: String
+    
+    var body: some View {
+        VStack{
+            Text(text)
+            HStack{
+                Image(systemName: "smiley")
+                Text("This is a SwiftUI View")
+            }
+        }
+        .font(.largeTitle)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView(text: "Sample Text")
+    }
+}
